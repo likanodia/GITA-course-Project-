97dbea2a4480d89172fad1e4fb81f1507f0d34c1
Lika Nodia <101971071+likanodia@users.noreply.github.com>
2022-09-18 01:14:16 +0400

    add hover effects

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#flip-dress").click(function(){
+      $("#panel-dress").slideToggle("fast");
+    });
+    $("#flip-blouse").click(function(){
+        $("#panel-blouse").slideToggle("fast");
+    });
+    $("#flip-cardigans").click(function(){
+        $("#panel-cardigans").slideToggle("fast");
+    });
+    $("#flip-skirts").click(function(){
+        $("#panel-skirts").slideToggle("fast");
+    });
+  });
